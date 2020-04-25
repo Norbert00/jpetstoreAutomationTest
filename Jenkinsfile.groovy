9ffe0e079808b9b2c1c70476ef1320355da4f614
Norbert <nocnica@protonamil.com>
2020-04-25 15:04:50 +0200

    add docker-compose for selenium grid on docker

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build test code') {
+            steps {
+                sh 'mvn clean install -DskipTests'
+            }
+        }
+        stage('Run selenium grid') {
+            steps {
+                sh 'docker-compose up -d'
+            }
+        }
+        stage('Execute test') {
+            steps {
+                sh 'mvn test'
+                sh 'docker-compose down'
+            }
+        }
+    }
+    post {
+        always {
+            script {
+                allure([
+                        includeProperties: false,
+                        jdk              : '',
+                        properties       : [],
+                        reportBuildPolicy: 'ALWAYS',
+                        results          : [[path: 'target/allure-results']]
+                ])
+            }
+        }
+    }
+}
